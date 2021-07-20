72cc3b933746907273dd5e5c2daf108293a45a78
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-07-20 06:18:15 +0200

    first commit

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,36 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+  
+  switch(color){
+    case "black" : 0;
+    break;
+    case "brown": 1;
+    break;
+    case "red": 2;
+    break;
+    case "orange": 3;
+    break;
+    case "yellow": 4;
+    break;
+    case "green": 5;
+    break;
+    case "blue": 6;
+    break;
+    case "violet": 7;
+    break;
+    case "grey": 8;
+    break;
+    case "white": 9;
+    break;
+    default: console.log('Sorry, we don\'t know that color');
+
+  }
+  console.log(`The color band number are ${color}`);
+};
+
+export const COLORS = undefined;
+
